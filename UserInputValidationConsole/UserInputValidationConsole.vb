13b4cd33d2910ae1c7464239e85b7846cf70ab50
Zac <zac.minidude140@Gmail.com>
2023-08-30 08:35:10 -0600

    Add TO DO plan

--- UserInputValidationConsole/UserInputValidationConsole.vb
+++ UserInputValidationConsole/UserInputValidationConsole.vb
@@ -4,6 +4,16 @@ Option Strict On
 Module UserInputValidationConsole
 
     Sub Main()
+        'TO DO
+        'Prompt user for number between 1 and 10 or "q" to quit
+        'reflect the user input to console
+        'Check if the input is a number
+        'if user enteted "q" or "Q" then quit the program
+        'Check if the input is within bounds
+        'Continue "loop" until the user uneter a valid number or enters "q"
+        'If yes congradulate the user
+
+
 
     End Sub
 
